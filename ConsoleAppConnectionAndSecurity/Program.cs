9c42c2e000b07c45fe68846343bae33c41c9f804
Ismoil Muhammadiev <ism0il.muhammadiev92@gmail.com>
2025-09-25 23:18:36 +0500

    Практика номер 6. Connection and Security.

---
+++ ConsoleAppConnectionAndSecurity/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleAppConnectionAndSecurity;
+
+Console.WriteLine("Выберите один из трёх заданий");
+Console.WriteLine("1- Подключиться к Wikipedia по HTTPS протоколу.");
+Console.WriteLine("2- Настроить заголовки авторизации для HTTP подключения.");
+Console.WriteLine("3- Создать  клиент  и  сервер  на  основе  TCP  протокола,  при  этом  организовать  многопоточное подключение клиентов.");
+Console.Write("Введите номер задании:");
+int numberTask = Convert.ToInt32(Console.ReadLine());
+
+switch(numberTask)
+{
+    case 1:
+        Tcp_Http_HTTPS_Protocols.TaskOneHTTPS_Protocol();
+        break;
+    case 2:
+        Tcp_Http_HTTPS_Protocols.TaskTwoHTTP_Protocol();
+        break;
+    case 3:
+        Tcp_Http_HTTPS_Protocols.TaskThreeTCP_Protocol();
+        break;
+    default:
+        Console.WriteLine("Incorrect number task");
+        break;
+}
